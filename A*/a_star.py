a97b5ada6189c1085a68b476a4778802aace3241
HeinVS <HvS1992@hotmail.com>
2014-12-08 23:48:47 +0100

    nogmaals bijna klaar

---
+++ A*/a_star.py
@@ -0,0 +1,238 @@
+try:
+    import Queue as Q  # ver. < 3.0
+except ImportError:
+    import queue as Q
+from GridMatrix import GridMatrix
+
+class a_star():
+
+    def __init__(self, x, y):
+        # self.matrix = matrix2
+        # self.layers = [matrix1, matrix2]
+        # self.dim_x = len(self.matrix[0])
+        # self.dim_y = len(self.matrix)
+        matrix_temp1 = GridMatrix(x, y)
+        self.matrix = list(matrix_temp1.list_ver)
+        matrix_temp2 = GridMatrix(x, y)
+        matrix_temp2.read_coordinates('grid_1.txt')
+        self.layers = [list(matrix_temp2.list_ver)]
+        self.dim_x = x
+        self.dim_y = y
+
+
+    def new_line(self, start, goal, value):
+        # nodes evaluated a dictionary with every node represented by their label and shortest route towards it
+        closedset =  {start : [start]}
+        
+        # cost from start along best known path.
+        steps = 0
+
+        first = node(start, 0, goal, steps, self.dim_x, self.dim_y, self.layers, list(self.matrix))
+        # list of potential nodes weighed by their score (sorted list)
+        openset = Q.PriorityQueue()
+        openset.put((first.node_score, first))
+
+        while not openset.empty():
+
+            current = openset.get()
+            current = current[1]
+            path = list(closedset[current.label])
+
+            # print "node:", current.node, "x:", current.node_x, "y:", current.node_y
+            # if the current node is the same as the goal
+            if current.node == goal:
+                # get the path from the closed set
+                path = closedset[current.label]
+                for i in path:
+                    if i == start or i == goal:
+                        continue
+                    x = self.calc_x(i)
+                    z = self.calc_z(i)
+                    y = self.calc_y(i, z)
+                    list_temp = list(self.layers[z][y])
+                    self.layers[z].pop(y)
+                    list_temp.pop(x)
+                    list_temp.insert(x, value)
+                    self.layers[z].insert(y, list_temp)
+                print self.layers
+                break
+
+            # you make a step so update the score by 1
+            amount_of_steps = current.steps + 1
+
+            # print current.label, path
+            
+            # go over the accompaniying nodes
+            for i in current.sides:
+
+                # store their attributes
+                i = node(i[0], i[1], goal, amount_of_steps, self.dim_x, self.dim_y, self.layers, list(self.matrix))
+                temp_path = list(path)
+                temp_path.append(i.label)         
+
+                # if the neighbor is already valued skip it
+                if i.label in closedset:
+                    continue
+                # otherwise put it in the closedset
+                else:
+                    closedset[i.label] = temp_path
+                    openset.put((i.node_score, i))
+
+        # if for some reason it steps out of the loop without returning a path alert
+        return False
+
+    def calc_x(self, node):
+        return (node % self.dim_x)
+
+    def calc_y(self, node, z):
+        return (int((node - (z * 1000)) / self.dim_x))
+
+    def calc_z(self,node):
+        return int(node / 1000)
+
+class node():
+        
+    def __init__(self, target, z, goal, steps, dim_x_m, dim_y_m, layers, matrix):
+        self.dim_x_m = dim_x_m
+        self.dim_y_m = dim_y_m
+        self.goal_x = self.calc_x(goal)
+        self.goal_y = self.calc_y(goal)
+        self.goal_z = 0
+        self.node = target
+        self.steps = steps
+        self.node_x = self.calc_x(self.node)
+        if self.node_x > self.dim_x_m - 1:
+            raise "this is not good"
+        self.node_y = self.calc_y(self.node)
+        if self.node_y > self.dim_y_m - 1:
+            raise "a little bit better"
+        self.node_layer = z 
+        self.label = self.node_layer * 1000 + self.node
+        self.matrix = list(matrix)
+        self.layers = layers
+        self.node_score = self.score(steps)
+        self.sides = self.neighbors()
+
+    def calc_x(self, node):
+        return (node % (self.dim_x_m))
+
+    def calc_y(self, node):
+        return (int(node / (self.dim_x_m)))
+
+    def calc_node(self, x, y):
+        return x + y * self.dim_x_m
+
+    def neighbors(self):
+        side_nodes = list()
+        right_node = [self.calc_node(self.node_x + 1, self.node_y), self.node_layer]
+        left_node = [self.calc_node(self.node_x - 1, self.node_y), self.node_layer]
+        above_node = [self.calc_node(self.node_x, self.node_y - 1), self.node_layer]
+        below_node = [self.calc_node(self.node_x, self.node_y + 1), self.node_layer]
+        layer_up_node = [self.calc_node(self.node_x, self.node_y), self.node_layer - 1]
+        layer_down_node = [self.calc_node(self.node_x, self.node_y), self.node_layer + 1]
+        place = self.place_node()
+        if place == 0:
+            side_nodes.append(right_node)
+            side_nodes.append(left_node)
+            side_nodes.append(above_node)
+            side_nodes.append(below_node)
+        elif place == 1:
+            side_nodes.append(left_node)
+            side_nodes.append(below_node)
+        elif place == 2: 
+            side_nodes.append(left_node)
+            side_nodes.append(above_node)
+        elif place == 3:
+            side_nodes.append(left_node)
+            side_nodes.append(above_node)
+            side_nodes.append(below_node)
+        elif place == 4:
+            side_nodes.append(right_node)
+            side_nodes.append(below_node)
+        elif place == 5:
+            side_nodes.append(right_node)
+            side_nodes.append(above_node)
+        elif place == 6:
+            side_nodes.append(right_node)
+            side_nodes.append(above_node)
+            side_nodes.append(below_node)
+        elif place == 7:
+            side_nodes.append(right_node)
+            side_nodes.append(left_node)
+            side_nodes.append(below_node)
+        elif place == 8:
+            side_nodes.append(right_node)
+            side_nodes.append(left_node)
+            side_nodes.append(above_node)
+        else:
+            print "JOEEEEEEEE"
+
+        if self.node_layer <= 0:
+            #side_nodes.append("") 
+            side_nodes.append(layer_down_node)
+        elif self.node_layer >= 7:
+            side_nodes.append(layer_up_node)
+            #side_nodes.append("")
+        else:
+            side_nodes.append(layer_up_node)
+            side_nodes.append(layer_down_node)
+        
+
+        return side_nodes
+
+
+    def place_node(self):
+        # right upper corner = 1, right lower corner = 2, right side = 3
+        # left upper corner = 4, left lower corner = 5, left side = 6
+        # upper side = 7, lower side = 8, not a side node = 0
+
+        # check if on the right
+        if self.node_x >= self.dim_x_m - 1:
+            # check for upper side
+            if self.node_y <= 0:
+                return 1
+
+            #check for lowerside
+            elif self.node_y >= self.dim_y_m - 1:
+                return 2
+            else:
+                return 3
+
+        # check for left side
+        elif self.node_x <= 0:
+             # check for upper side
+            if self.node_y <= 0:
+                return 4
+
+            #check for lowerside
+            elif self.node_y >= self.dim_y_m - 1:
+                return 5
+
+            else: 
+                return 6
+
+        # check for upperside
+        elif self.node_y <= 0:
+            return 7
+
+        # check for lowerside
+        elif self.node_y >= self.dim_y_m - 1:
+            return 8
+
+        else:
+            return 0
+
+    def score(self, current_score):
+        score = current_score + self.mh_dis()
+        if self.node_layer > (len(self.layers) - 1):
+            self.layers.append(list(self.matrix))
+
+        if self.layers[self.node_layer][self.node_y][self.node_x] != 0:
+            score = 100000
+        return score
+
+    def mh_dis(self):
+        xdis = abs(self.goal_x - self.node_x)
+        ydis = abs(self.goal_y - self.node_y)
+        zdis = abs(self.goal_z - self.node_layer)
+        return (xdis + ydis + zdis)
